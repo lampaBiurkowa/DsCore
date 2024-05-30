3fb9d8f205c693a882db480ddb08a0d55740fecb
hnoj <mrocznaklawiatura@gmail.com>
2024-05-30 21:14:44 +0200

    :|

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -4,6 +4,21 @@ using DsCryptoLib;
 
 public class Generator
 {
+    static User GetUser(string alias, string email, string surname, string name, string profile)
+    {
+        return new()
+        {
+            Alias = alias,
+            Email = email,
+            Surname = surname,
+            Name = name,
+            ProfileImage = profile,
+            LastOnline = DateTime.Now,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        };
+    }
+
     public static void Main()
     {
         var db = new DsCoreContext();
@@ -12,20 +27,20 @@ public class Generator
 
         db.Currency.Add(new() { Name = "Ruble"});
 
-        db.User.Add(new () { Alias = "d", Email = "d@d.d", Surname = "d", Name = "d", ProfileImage="wawrzyn1.png"});
-        db.User.Add(new () { Alias = "e", Email = "e@e.e", Surname = "e", Name = "e", ProfileImage="npc.png"});
-        db.User.Add(new () { Alias = "suhy", Email = "f@d.d", Surname = "Suchojad", Name = "Krzysztof", ProfileImage="suhojad.png"});
-        db.User.Add(new () { Alias = "ct", Email = "g@d.d", Surname = "Tokarczyk", Name = "Cezary", ProfileImage="tokar.png"});
-        db.User.Add(new () { Alias = "wawrzyn", Email = "h@d.d", Surname = "Milkiewicz", Name = "Wawrzyniec", ProfileImage="wawrzyn.jpg"});
-        db.User.Add(new () { Alias = "kierecki", Email = "i@d.d", Surname = "Kierecki", Name = "Rafał", ProfileImage="kierecki.png"});
-        db.User.Add(new () { Alias = "koziel", Email = "j@d.d", Surname = "Kozieł", Name = "Kacper", ProfileImage="koziel.png"});
-        db.User.Add(new () { Alias = "cadi", Email = "k@d.d", Surname = "Kadziewicz", Name = "Mikołaj", ProfileImage="cadi.jpeg"});
-        db.User.Add(new () { Alias = "karton", Email = "l@d.d", Surname = "Pospolity", Name = "Karton", ProfileImage="chad.png"});
-        db.User.Add(new () { Alias = "julzal", Email = "m@d.d", Surname = "Zalewski", Name = "Julian", ProfileImage="angry.jpg"});
-        db.User.Add(new () { Alias = "kjub", Email = "n@d.d", Surname = "Głuszek", Name = "Jakub", ProfileImage="en57.jpg"});
-        db.User.Add(new () { Alias = "biden", Email = "o@d.d", Surname = "Biden", Name = "Joe", ProfileImage="biden.jpg"});
-        db.User.Add(new () { Alias = "trump", Email = "p@d.d", Surname = "Trump", Name = "Donald J", ProfileImage="trump.jpg"});
-        db.User.Add(new () { Alias = "sussy", Email = "q@d.d", Surname = "Supińska", Name = "Karolina", ProfileImage="suspinska.png"});
+        db.User.Add(GetUser("d", "d@d.d",  "d", "d", "wawrzyn1.png"));
+        db.User.Add(GetUser("e", "e@e.e",  "e", "e", "npc.png"));
+        db.User.Add(GetUser("suhy", "f@d.d",  "Suchojad", "Krzysztof", "suhojad.png"));
+        db.User.Add(GetUser("ct", "g@d.d",  "Tokarczyk", "Cezary", "tokar.png"));
+        db.User.Add(GetUser("wawrzyn", "h@d.d",  "Milkiewicz", "Wawrzyniec", "wawrzyn.jpg"));
+        db.User.Add(GetUser("kierecki", "i@d.d",  "Kierecki", "Rafał", "kierecki.png"));
+        db.User.Add(GetUser("koziel", "j@d.d",  "Kozieł", "Kacper", "koziel.png"));
+        db.User.Add(GetUser("cadi", "k@d.d",  "Kadziewicz", "Mikołaj", "cadi.jpeg"));
+        db.User.Add(GetUser("karton", "l@d.d",  "Pospolity", "Karton", "chad.png"));
+        db.User.Add(GetUser("julzal", "m@d.d",  "Zalewski", "Julian", "angry.jpg"));
+        db.User.Add(GetUser("kjub", "n@d.d",  "Głuszek", "Jakub", "en57.jpg"));
+        db.User.Add(GetUser("biden", "o@d.d",  "Biden", "Joe", "biden.jpg"));
+        db.User.Add(GetUser("trump", "p@d.d",  "Trump", "Donald J", "trump.jpg"));
+        db.User.Add(GetUser("sussy", "q@d.d",  "Supińska", "Karolina", "suspinska.png"));
         db.SaveChanges();
 
         for (int i = 0; i < db.User.Count(); i++)
