ae740a5c6cef1476b78820c92153b5847a31ffbc
hnoj <mrocznaklawiatura@gmail.com>
2024-05-31 00:15:37 +0200

    nugets

--- DsCore.ApiClient/DsCoreClient.cs
+++ DsCore.ApiClient/DsCoreClient.cs
@@ -311,6 +311,92 @@ namespace DsCore.ApiClient
         }
 
         /// <exception cref="ApiException">A server side error occurred.</exception>
+        public virtual System.Threading.Tasks.Task<FileResponse> Auth_RegenerateCodeAsync(System.Guid userGuid, string verificationCodeBase64)
+        {
+            return Auth_RegenerateCodeAsync(userGuid, verificationCodeBase64, System.Threading.CancellationToken.None);
+        }
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public virtual async System.Threading.Tasks.Task<FileResponse> Auth_RegenerateCodeAsync(System.Guid userGuid, string verificationCodeBase64, System.Threading.CancellationToken cancellationToken)
+        {
+            if (userGuid == null)
+                throw new System.ArgumentNullException("userGuid");
+
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Content = new System.Net.Http.StringContent(string.Empty, System.Text.Encoding.UTF8, "application/octet-stream");
+                    request_.Method = new System.Net.Http.HttpMethod("POST");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/octet-stream"));
+
+                    var urlBuilder_ = new System.Text.StringBuilder();
+                
+                    // Operation Path: "Auth/activate/{userGuid}/regenerate-code"
+                    urlBuilder_.Append("Auth/activate/");
+                    urlBuilder_.Append(System.Uri.EscapeDataString(ConvertToString(userGuid, System.Globalization.CultureInfo.InvariantCulture)));
+                    urlBuilder_.Append("/regenerate-code");
+                    urlBuilder_.Append('?');
+                    if (verificationCodeBase64 != null)
+                    {
+                        urlBuilder_.Append(System.Uri.EscapeDataString("verificationCodeBase64")).Append('=').Append(System.Uri.EscapeDataString(ConvertToString(verificationCodeBase64, System.Globalization.CultureInfo.InvariantCulture))).Append('&');
+                    }
+                    urlBuilder_.Length--;
+
+                    PrepareRequest(client_, request_, urlBuilder_);
+
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+
+                    PrepareRequest(client_, request_, url_);
+
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.IEnumerable<string>>();
+                        foreach (var item_ in response_.Headers)
+                            headers_[item_.Key] = item_.Value;
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+
+                        ProcessResponse(client_, response_);
+
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200 || status_ == 206)
+                        {
+                            var responseStream_ = response_.Content == null ? System.IO.Stream.Null : await response_.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                            var fileResponse_ = new FileResponse(status_, headers_, responseStream_, null, response_);
+                            disposeClient_ = false; disposeResponse_ = false; // response and client are disposed by FileResponse
+                            return fileResponse_;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+
+        /// <exception cref="ApiException">A server side error occurred.</exception>
         public virtual System.Threading.Tasks.Task<FileResponse> Auth_ChangePasswordAsync(System.Guid userGuid, string oldPasswordBase64, string newPasswordBase64)
         {
             return Auth_ChangePasswordAsync(userGuid, oldPasswordBase64, newPasswordBase64, System.Threading.CancellationToken.None);
@@ -2295,6 +2381,10 @@ namespace DsCore.ApiClient
         [Newtonsoft.Json.JsonProperty("name", Required = Newtonsoft.Json.Required.Always)]
         [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
         public string Name { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("symbol", Required = Newtonsoft.Json.Required.Always)]
+        [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+        public string Symbol { get; set; }
 
     }
 
